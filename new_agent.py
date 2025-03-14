b8b434cc3c56cd4d9d1387ef05c5c1de850e09d0
Vidhu <totallyrealemail@oneofthese.yo>
2025-03-14 23:10:27 +0530

    fix: check logs, move agent descriptions to prompts

--- new_agent.py
+++ new_agent.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 
 import logging
-logging.basicConfig(filename="./logs/gemini2.log", level=logging.WARNING, format="%(asctime)s - %(message)s")
+logging.basicConfig(filename="./logs/gemini2.log", level=logging.INFO, format="%(asctime)s - %(message)s")
 
 _ = os.getenv('TEAM_API_KEY')
 gemini2_id = os.getenv('GEMINI2_FLASH_ID')
@@ -13,21 +13,13 @@ if not gemini2_id:
 from aixplain.factories import AgentFactory
 from aixplain.modules.agent import OutputFormat
 
+agent_description: str | None
+with open('prompts/001_agent_desc.txt', 'r') as file:
+    agent_description = file.read()
+
 agent = AgentFactory.create(
     name='Personalized Teacher',
-    description="""
-    You are a teacher of a 11 grade student studying in the Indian School
-    Board of CBSE. You need to answer the students questions diligently,
-    succinctly and in a simple and understandable manner.
-
-    You need to terse with you answers and be to the point. Do not go off
-    topic and blabber around too much. Stick extremely closely to the NCERT
-    books as prescribed by the board. These are standard books in the board's
-    curriculum and are also freely available online.
-
-    If the student starts to question something outside of the syllabus or
-    acts in a bad fashion then correct the student and focus on begin on track.
-    """,
+    description=agent_description,
     tools=[
         AgentFactory.create_model_tool(model=gemini2_id),
     ],
